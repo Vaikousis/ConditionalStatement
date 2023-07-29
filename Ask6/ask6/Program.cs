ef5fd59172cf265151bb1aa31d5fa0eefc4ce8d7
Vaikousis <140890559+Vaikousis@users.noreply.github.com>
2023-07-29 14:07:08 +0300

    Add files via upload

---
+++ Ask6/ask6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+using System.Xml.Linq;
+
+namespace Ask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Hector!");
+            float bk;
+
+            Console.WriteLine("poso baumous kelsiou exeis?");
+            bk = float.Parse(Console.ReadLine());
+
+            if (bk >= 35.5 && bk < 37)
+            {
+                Console.WriteLine("Fusiologikos");
+            }
+            else if (bk >= 37 && bk < 38)
+            {
+                Console.WriteLine("zestos");
+            }
+            else if (bk >= 38 && bk < 42)
+            {
+                Console.WriteLine("Arostos");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
---
+++ Ask7/Ask7/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Ask7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            double leutaLogariasmou, posaLeutaUaBgaleis;
+            double sunolo;
+
+            Console.WriteLine("posa leuta exeis ston logargiasmo sou?");
+            leutaLogariasmou = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("posa leuta ua bgaleis apo to ATM?");
+            posaLeutaUaBgaleis = float.Parse(Console.ReadLine());
+
+            double promiuia = 1.0/100 * posaLeutaUaBgaleis;
+
+            if (promiuia < 1)
+            {
+                promiuia = 1;
+            }
+            else if (promiuia > 3)
+            {
+                promiuia = 3;
+
+            }
+
+            sunolo = posaLeutaUaBgaleis +  promiuia;
+
+            if (leutaLogariasmou >= sunolo)
+
+            {
+                Console.WriteLine("EBGALES " + sunolo + " euro");
+                leutaLogariasmou -= sunolo;
+                Console.WriteLine("H promiuia einai afth = " + promiuia);
+                Console.WriteLine("kai to sunolo pou thasas exei minei ston logariasmo einai = " + leutaLogariasmou);
+            }
+            else
+            {
+                Console.WriteLine("den mporei na pragmatopithh");
+            }
+        }
+    }
+}
