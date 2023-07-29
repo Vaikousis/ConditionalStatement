88c7209ae9b249ed403db838af9157e18e98ffac
Vaikousis <140890559+Vaikousis@users.noreply.github.com>
2023-07-29 14:06:19 +0300

    Add files via upload

---
+++ Ask3/Ask3/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Ask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Hector!");
+
+            // ask3
+
+            string name1, name2;
+            int tall1, tall2;
+
+            Console.WriteLine("Write a basketball player name ");
+            name1 = Console.ReadLine();
+
+            Console.WriteLine("Write " + name1 + " hight");
+            tall1 =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write another  basketball player name ");
+            name2 = Console.ReadLine();
+
+            Console.WriteLine("Write " + name2 + " hight");
+            tall2 = int.Parse(Console.ReadLine());
+
+            if (tall1 > tall2)
+            {
+                Console.WriteLine(name1 + " is taller than " + name2);
+            }
+            else if  (tall1 < tall2)
+            {
+               Console.WriteLine(name2 + " is taller than " + name1);
+            }
+            else if (tall2 == tall1)
+            {
+                Console.WriteLine("They are the same hight");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ask4/Ask4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+namespace Ask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Hector!");
+
+            // ask4
+
+            float number ;
+
+            Console.WriteLine("Write a  number ");
+            number = float.Parse (Console.ReadLine());
+
+            if (number < 0) 
+            {
+                Console.WriteLine(number + number * -2);
+                //Console.WriteLine(Math.Abs(number));
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+            
+        }
+    }
+} 
